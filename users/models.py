c28141fbb06b188f632aa9ee7fb245b0ff20be3b
Sachin Sharma <vsharma3973@gmail.com>
2021-01-12 23:20:03 +0530

    First Commit.

---
+++ users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    email = models.EmailField(null=False, unique=True, blank=False)
+    phone_num = models.CharField(max_length=25)
+    date_of_birth = models.DateField(null=True, blank=True)
